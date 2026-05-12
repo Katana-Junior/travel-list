045d1abb751e44edfb07963b8c877843135531fa
John Katana <johnkatana624@gmail.com>
2026-05-12 16:18:34 +0300

    todays

--- src/App.js
+++ src/App.js
@@ -123,6 +123,12 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   return (
     <div className="items">
       <li style={item.packed ? { textDecoration: "line-through" } : {}}>
+        <input
+          type="checkbox"
+          checked={item.packed}
+          onChange={() => onToggleItem(item.id)}
+          className="check-box"
+        />
         <span onClick={() => onToggleItem(item.id)}>{item.description}</span>
         <span> {item.quantity}</span>
         <button className="delete-btn" onClick={() => onDeleteItem(item.id)}>
